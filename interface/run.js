0041fdb4bd037ba204c7b036f9a334c65c22b33b
Ramana Kumar <ramana@member.fsf.org>
2023-03-06 21:45:59 +0000

    Add sending ether functionality

--- interface/run.js
+++ interface/run.js
@@ -25,33 +25,105 @@ server.listen(process.env.PORT || 8080)
   console.log(`Connected to ${JSON.stringify(network)}`)
 
   const game = new ethers.Contract(process.env.GAME,
-    JSON.parse(fs.readFileSync(process.env.GAME_ABI || '../interfaces/Game.json', 'utf8')))
+    JSON.parse(fs.readFileSync(process.env.GAME_ABI || '../interfaces/Game.json', 'utf8')),
+    provider)
+  console.log(`Game is ${game.address}`)
+  const room = new ethers.Contract(process.env.ROOM,
+    JSON.parse(fs.readFileSync(process.env.ROOM_ABI || '../interfaces/Room.json', 'utf8')),
+    provider)
+  console.log(`Room is ${room.address}`)
+
+  async function refreshBalance(socket) {
+    async function b() {
+      console.log(`querying balance for ${socket.account.address}...`)
+      const bal = await provider.getBalance(socket.account.address)
+      console.log(`...done`)
+      return bal
+    }
+    socket.emit('balance',
+      socket.account && socket.account.privateKey != ''
+      ? ethers.utils.formatUnits(await b(), 'ether')
+      : '')
+  }
 
   async function changeAccount(socket) {
     socket.emit('account', socket.account.address, socket.account.privateKey)
+    await refreshBalance(socket)
     if (socket.account.privateKey != '') {
-      socket.emit('balance', ethers.utils.formatUnits(await provider.getBalance(socket.account.address), 'ether'))
-      // check which games this address is currently in and display them all
-    }
-    else {
-      socket.emit('balance', '')
+      console.log(`querying joined games for ${socket.account.address}...`)
+      const joins = await room.queryFilter(
+        room.filters.JoinTable(null, socket.account.address, null),
+        -10 // TODO configure this properly
+      )
+      console.log(`filtering left games...`)
+      const liveJoins = (await Promise.all(joins.map(ev =>
+        room.queryFilter(
+          room.filters.LeaveTable(ev.args.table, ev.args.player, ev.args.seat),
+          ev.blockNumber)
+        .then(a => a.length ? null : ev)))).filter(x => x)
+      console.log(`checking which games have started...`)
+      const pendingStarted = (await Promise.all(liveJoins.map(ev =>
+        room.queryFilter(
+          room.filters.StartGame(ev.args.table),
+          ev.blockNumber)
+        .then(a => [a.length, ev]))))
+      .reduce((acc, tup) => acc[tup[0] ? 1 : 0].push(tup[1]) && acc, [[], []])
+      console.log(`filtering finished games...`)
+      const active = (await Promise.all(pendingStarted[1].map(ev =>
+        room.queryFilter(
+          room.filters.EndGame(ev.args.table),
+          ev.blockNumber)
+        .then(a => a.length ? null : ev)))).filter(x => x)
+      console.log(`...done`)
+      pendingStarted[0].forEach(ev => {
+        socket.emit('waiting', ev.args.table)
+      })
+      active.forEach(ev => {
+        socket.emit('playing', ev.args.table)
+      })
     }
   }
 
   io.on('connection', socket => {
     socket.on('newAccount', async () => {
-      socket.account = ethers.Wallet.createRandom()
+      socket.account = ethers.Wallet.createRandom().connect(provider)
       await changeAccount(socket)
+    })
+
+    socket.on('refreshBalance', async () => {
+      await refreshBalance(socket)
     })
 
     socket.on('privkey', async privkey => {
       try {
-        socket.account = new ethers.Wallet(privkey)
+        socket.account = new ethers.Wallet(privkey, provider)
       }
       catch {
         socket.account = {address: '', privateKey: ''}
       }
       await changeAccount(socket)
+    })
+
+    socket.on('send', async (to, amount) => {
+      try {
+        console.log('entered send function')
+        const tx = {
+          to: to,
+          type: 2,
+          maxFeePerGas: ethers.utils.parseUnits('16', 'gwei'), // TODO
+          maxPriorityFeePerGas: ethers.utils.parseUnits('2', 'gwei'), // TODO
+          value: ethers.utils.parseEther(amount)
+        }
+        console.log(`sending ${amount} to ${to} from ${socket.account.address}...`)
+        const response = await socket.account.sendTransaction(tx)
+        console.log(`awaiting receipt...`)
+        const receipt = await response.wait()
+        console.log(`...done`)
+        await refreshBalance(socket)
+      }
+      catch (e) {
+        socket.emit('sendError', e.toString())
+      }
     })
   })
 
--- interface/script.js
+++ interface/script.js
@@ -5,6 +5,9 @@ const privkeyElement = document.getElementById('privkey')
 const newAccountButton = addressElement.parentElement.previousElementSibling
 const hidePrivkeyButton = privkeyElement.previousElementSibling
 
+const joinDiv = document.getElementById('joinDiv')
+const playDiv = document.getElementById('playDiv')
+
 hidePrivkeyButton.addEventListener('click', (e) => {
   if (privkeyElement.classList.contains('hidden')) {
     privkeyElement.classList.remove('hidden')
@@ -29,12 +32,44 @@ socket.on('account', (address, privkey) => {
   addressElement.value = address
   privkeyElement.value = privkey
   newAccountButton.disabled = privkeyElement.value != ''
+  joinDiv.replaceChildren()
+  playDiv.replaceChildren()
 })
 
 privkeyElement.dispatchEvent(new Event('change'))
 
 const balanceElement = document.getElementById('balance')
+const refreshBalanceButton = document.getElementById('refreshBalance')
+const sendAmountElement = document.getElementById('sendAmount')
+const sendToElement = document.getElementById('sendTo')
+const sendButton = document.getElementById('sendButton')
+
+refreshBalanceButton.addEventListener('click', (e) => {
+  socket.emit('refreshBalance')
+})
+
+sendButton.addEventListener('click', (e) => {
+  if (sendToElement.checkValidity() && sendAmountElement.checkValidity()) {
+    socket.emit('send', sendToElement.value, sendAmountElement.value)
+  }
+  else {
+    sendToElement.reportValidity()
+    sendAmountElement.reportValidity()
+  }
+})
 
 socket.on('balance', balance => {
   balanceElement.value = balance
+})
+
+socket.on('waiting', tableId => {
+  joinDiv.appendChild(document.createElement('p')).innerText = `Waiting in ${tableId}`
+})
+
+socket.on('playing', tableId => {
+  playDiv.appendChild(document.createElement('p')).innerText = `Playing in ${tableId}`
+})
+
+socket.on('sendError', msg => {
+  sendToElement.value = msg
 })
